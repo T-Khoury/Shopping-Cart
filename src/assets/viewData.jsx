1cf02766e86ebaa9eb41b2dd51d44bf92be861a3
T-Khoury <120677926+T-Khoury@users.noreply.github.com>
2024-01-16 11:17:43 -0500

    Rename data file and add sorted view data

---
+++ src/assets/viewData.jsx
@@ -0,0 +1,29 @@
+const genres = [
+    {name: "Action", slug: "action"},
+    {name: "Indie", slug: "indie"},
+    {name: "Adventure", slug: "adventure"},
+    {name: "RPG", slug: "role-playing-games-rpg"},
+    {name: "Strategy", slug: "strategy"},
+    {name: "Shooter", slug: "shooter"},
+    {name: "Casual", slug: "casual"},
+    {name: "Simulation", slug: "simulation"},
+    {name: "Puzzle", slug: "puzzle"},
+    {name: "Arcade", slug: "arcade"},
+    {name: "Platformer", slug: "platformer"},
+    {name: "Massively Multiplayer", slug: "massively-multiplayer"},
+    {name: "Racing", slug: "racing"},
+    {name: "Sports", slug: "sports"},
+    {name: "Fighting", slug: "fighting"},
+    {name: "Family", slug: "family"},
+    {name: "Board Games", slug: "board-games"},
+    {name: "Education", slug: "educational"},
+    {name: "Card", slug: "card"}
+]
+
+const sorted = [
+    {name: "Top Rated", type: "top-rated"},
+    {name: "Popular", type: "popular"},
+    {name: "Upcoming", type: "upcoming"}
+]
+
+export { genres, sorted }
