8463a04a8a9c0949e1673784fed3d1f4452dfcc6
T-Khoury <120677926+T-Khoury@users.noreply.github.com>
2024-03-25 16:58:30 -0400

    Add more details to game detail page

--- src/components/GameDetail.jsx
+++ src/components/GameDetail.jsx
@@ -4,6 +4,18 @@ import { getGamePrice } from "./Shop";
 
 const key = "90cc2d37097041038ea82cf6fc69d6d5";
 
+function removeTags(str) {
+    if ((str === null) || (str === ''))
+        return false;
+    else
+        str = str.toString();
+ 
+    // Regular expression to identify HTML tags in
+    // the input string. Replacing the identified
+    // HTML tag with a null string.
+    return str.replace(/(<([^>]+)>)/ig, '');
+}
+
 
 const useData = (gameID) => {
     const [data, setData] = useState(null);
@@ -42,20 +54,48 @@ const GamePage = () => {
     const name = data.name;
     const image = data.background_image;
     const price = getGamePrice(data.metacritic, data.rating);
+    const releaseData = data.released;
+    const developer = data.developers[0].name;
+    const publisher = data.publishers[0].name;
+    const platforms = data.platforms.map((platform) => platform.platform.name)
     
-    console.log(cartItems);
+    console.log(data);
+    console.log(platforms);
 
 
     return (
         <div className="game-detail-container">
             <h1>{name}</h1>
-            <img src={image} />
-            <p>{data.description.slice(3, -4)}</p>
-            <div className="price-container">
-                <p>{price}</p>
-                <button onClick={() => handleCartButton(name, image, price, id)}>
-                Add to Cart
-            </button>
+            <div className="game-details">
+                <div className="game-details-left">
+                    <img className="game-detail-picture" src={image} />
+                    <p>{removeTags(data.description)}</p>
+                </div>
+                <div className="game-details-right">
+                    <p className="game-details-price">{price}</p>
+                    <button className="game-detail-buynow-button">Buy Now</button>
+                    <button className="game-detail-cart-button" onClick={() => handleCartButton(name, image, price, id)}>
+                    Add to Cart
+                    </button>
+                    <div className="more-info-container">
+                        <div className="detail-container">
+                            <span className="detail-category">Developer</span>
+                            <span className="detail-info">{developer}</span>
+                        </div>
+                        <div className="detail-container">
+                            <span className="detail-category">Publisher</span>
+                            <span className="detail-info">{publisher}</span>
+                        </div>
+                        <div className="detail-container">
+                            <span className="detail-category">Release Date</span>
+                            <span className="detail-info">{releaseData}</span>
+                        </div>
+                        <div className="detail-container">
+                            <span className="detail-category">Platform</span>
+                            <span className="detail-info">{platforms}</span>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
