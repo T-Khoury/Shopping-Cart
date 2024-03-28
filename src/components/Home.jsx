dddbaed8847cbc5caf51970dc363bc2500160bbe
T-Khoury <120677926+T-Khoury@users.noreply.github.com>
2024-03-28 09:21:50 -0400

    Add site-credits section to homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom"
+import githubLogo from "../assets/github.svg"
+import RAWGLogo from "../assets/RAWG.png"
 
 const Home = () => {
 
@@ -9,6 +11,16 @@ const Home = () => {
                 <h1 className="home-title">Game Shop</h1>
                 <p className="home-description">Your <span className="home-description-span">one-stop shop</span> for videogames</p>
                 <Link to={'/shop'}><button className="home-browse-button">Browse Collection</button></Link>
+            </div>
+            <div className="site-credits">
+                <a href="https://github.com/T-Khoury">
+                    <img src={githubLogo} />
+                    T-Khoury
+                </a>
+                <a href="https://rawg.io/apidocs">
+                    <img src={RAWGLogo} />
+                    RAWG API
+                </a>
             </div>
         </div>
     )
